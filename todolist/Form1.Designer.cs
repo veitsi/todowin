3d138fe60310b3a17636f0aee1726a0af52ada66
veitsi <xtfkpi@gmail.com>
2015-06-11 12:22:58 +0300

    not working

--- todolist/Form1.Designer.cs
+++ todolist/Form1.Designer.cs
@@ -32,18 +32,21 @@
             System.Windows.Forms.TreeNode treeNode2 = new System.Windows.Forms.TreeNode("Node4");
             System.Windows.Forms.TreeNode treeNode3 = new System.Windows.Forms.TreeNode("Node5");
             System.Windows.Forms.TreeNode treeNode4 = new System.Windows.Forms.TreeNode("Node6");
-            System.Windows.Forms.TreeNode treeNode5 = new System.Windows.Forms.TreeNode("Item0", new System.Windows.Forms.TreeNode[] {
+            System.Windows.Forms.TreeNode treeNode5 = new System.Windows.Forms.TreeNode("");
+            System.Windows.Forms.TreeNode treeNode6 = new System.Windows.Forms.TreeNode("Item0", new System.Windows.Forms.TreeNode[] {
             treeNode1,
             treeNode2,
             treeNode3,
-            treeNode4});
-            System.Windows.Forms.TreeNode treeNode6 = new System.Windows.Forms.TreeNode("Node7");
-            System.Windows.Forms.TreeNode treeNode7 = new System.Windows.Forms.TreeNode("Item1", new System.Windows.Forms.TreeNode[] {
-            treeNode6});
-            System.Windows.Forms.TreeNode treeNode8 = new System.Windows.Forms.TreeNode("Item2");
-            System.Windows.Forms.TreeNode treeNode9 = new System.Windows.Forms.TreeNode("Node8");
-            System.Windows.Forms.TreeNode treeNode10 = new System.Windows.Forms.TreeNode("Node9");
-            System.Windows.Forms.TreeNode treeNode11 = new System.Windows.Forms.TreeNode("Node10");
+            treeNode4,
+            treeNode5});
+            System.Windows.Forms.TreeNode treeNode7 = new System.Windows.Forms.TreeNode("Node7");
+            System.Windows.Forms.TreeNode treeNode8 = new System.Windows.Forms.TreeNode("Item1", new System.Windows.Forms.TreeNode[] {
+            treeNode7});
+            System.Windows.Forms.TreeNode treeNode9 = new System.Windows.Forms.TreeNode("Item2");
+            System.Windows.Forms.TreeNode treeNode10 = new System.Windows.Forms.TreeNode("Node8");
+            System.Windows.Forms.TreeNode treeNode11 = new System.Windows.Forms.TreeNode("Node9");
+            System.Windows.Forms.TreeNode treeNode12 = new System.Windows.Forms.TreeNode("Node10");
+            System.Windows.Forms.TreeNode treeNode13 = new System.Windows.Forms.TreeNode("Node12");
             this.todo = new System.Windows.Forms.TreeView();
             this.btnOpen = new System.Windows.Forms.Button();
             this.btnSave = new System.Windows.Forms.Button();
@@ -53,6 +56,8 @@
             // todo
             // 
             this.todo.CheckBoxes = true;
+            this.todo.ImeMode = System.Windows.Forms.ImeMode.Hiragana;
+            this.todo.LabelEdit = true;
             this.todo.Location = new System.Drawing.Point(13, 13);
             this.todo.Name = "todo";
             treeNode1.Name = "Node3";
@@ -63,43 +68,49 @@
             treeNode3.Text = "Node5";
             treeNode4.Name = "Node6";
             treeNode4.Text = "Node6";
-            treeNode5.Name = "Node0";
-            treeNode5.Text = "Item0";
-            treeNode6.Name = "Node7";
-            treeNode6.Text = "Node7";
-            treeNode7.Name = "node1";
-            treeNode7.Text = "Item1";
-            treeNode8.Name = "Node2";
-            treeNode8.Text = "Item2";
-            treeNode9.Name = "Node8";
-            treeNode9.Text = "Node8";
-            treeNode10.Name = "Node9";
-            treeNode10.Text = "Node9";
-            treeNode11.Name = "Node10";
-            treeNode11.Text = "Node10";
+            treeNode5.Name = "Node11";
+            treeNode5.Text = "";
+            treeNode6.Name = "Node0";
+            treeNode6.Text = "Item0";
+            treeNode7.Name = "Node7";
+            treeNode7.Text = "Node7";
+            treeNode8.Name = "node1";
+            treeNode8.Text = "Item1";
+            treeNode9.Name = "Node2";
+            treeNode9.Text = "Item2";
+            treeNode10.Name = "Node8";
+            treeNode10.Text = "Node8";
+            treeNode11.Name = "Node9";
+            treeNode11.Text = "Node9";
+            treeNode12.Name = "Node10";
+            treeNode12.Text = "Node10";
+            treeNode13.Name = "Node12";
+            treeNode13.Text = "Node12";
             this.todo.Nodes.AddRange(new System.Windows.Forms.TreeNode[] {
-            treeNode5,
-            treeNode7,
+            treeNode6,
             treeNode8,
             treeNode9,
             treeNode10,
-            treeNode11});
-            this.todo.Size = new System.Drawing.Size(427, 274);
+            treeNode11,
+            treeNode12,
+            treeNode13});
+            this.todo.Size = new System.Drawing.Size(427, 240);
             this.todo.TabIndex = 0;
             this.todo.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.todo_AfterSelect);
             // 
             // btnOpen
             // 
-            this.btnOpen.Location = new System.Drawing.Point(13, 324);
+            this.btnOpen.Location = new System.Drawing.Point(12, 272);
             this.btnOpen.Name = "btnOpen";
             this.btnOpen.Size = new System.Drawing.Size(213, 57);
             this.btnOpen.TabIndex = 1;
             this.btnOpen.Text = "Open";
             this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
             // 
             // btnSave
             // 
-            this.btnSave.Location = new System.Drawing.Point(232, 324);
+            this.btnSave.Location = new System.Drawing.Point(237, 272);
             this.btnSave.Name = "btnSave";
             this.btnSave.Size = new System.Drawing.Size(203, 57);
             this.btnSave.TabIndex = 2;
@@ -110,7 +121,7 @@
             // status
             // 
             this.status.AutoSize = true;
-            this.status.Location = new System.Drawing.Point(10, 290);
+            this.status.Location = new System.Drawing.Point(128, 256);
             this.status.Name = "status";
             this.status.Size = new System.Drawing.Size(167, 13);
             this.status.TabIndex = 3;
@@ -121,7 +132,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(447, 390);
+            this.ClientSize = new System.Drawing.Size(468, 333);
             this.Controls.Add(this.status);
             this.Controls.Add(this.btnSave);
             this.Controls.Add(this.btnOpen);
--- todolist/Form1.cs
+++ todolist/Form1.cs
@@ -24,11 +24,28 @@ namespace todolist
 
         private void btnSave_Click(object sender, EventArgs e)
         {
-            status.Text =""+ todo.Nodes.Count;
+            //status.Text =""+ todo.Nodes.Count;
+            Console.WriteLine("n=" + todo.Nodes.Count);
+            TreeNode node;
+            node = todo.Nodes[0];
+            todo.Nodes[0]
+                n
+            for (int i = 0; i < todo.Nodes.Count; i++)
+            {
+                
+                Console.WriteLine(todo.Nodes[i].Text);
+                for (int j = 0; j < todo.Nodes[i].GetNodeCount(false); j++)
+                    Console.WriteLine("--->"+todo.Nodes[i].Nodes[j].Text);
+            }
 
         }
 
         private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnOpen_Click(object sender, EventArgs e)
         {
 
         }
